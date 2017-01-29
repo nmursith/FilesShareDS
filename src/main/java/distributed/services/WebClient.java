ba588601de486936701f53603a1218c892938d12
UdeeshaShehan <udeesha.12@cse.mrt.ac.lk>
2017-01-29 15:06:56 +0530

    converted to maven

---
+++ src/main/java/distributed/services/WebClient.java
@@ -0,0 +1,7 @@
+package distributed.services;
+
+/**
+ * Created by Udeesha on 1/29/2017.
+ */
+public class WebClient {
+}
---
+++ src/main/java/distributed/services/WebService.java
@@ -0,0 +1,25 @@
+package distributed.services;
+
+import javax.jws.WebMethod;
+import javax.xml.ws.Endpoint;
+
+/**
+ * Created by Udeesha on 1/29/2017.
+ */
+@javax.jws.WebService
+public class WebService {
+    public static void main(String[] args) {
+        WebService wsInstance = new WebService();
+        Endpoint.publish("http://localhost:8282/tutorial", wsInstance);
+    }
+
+    @WebMethod
+    public String getHelloWorld() {
+        return "Hello, World!";
+    }
+
+    @WebMethod
+    public String getPersonalizedHelloWorld(String yourName) {
+        return "Hello, " + yourName + "!";
+    }
+}
